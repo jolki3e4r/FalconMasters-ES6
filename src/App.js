cf5bb6fe7ad525a2a9fe925babfb679a1a808dc3
jhonatan velez <talimon@outlook.com>
2019-09-26 16:34:07 -0500

    terminando el video de modulos

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { mensajeAlerta } from "./modulos/mensajeAlerta"
+import Persona from "./modulos/clasePersona"
+
+//mensajeAlerta("hola carlos");
+
+const carlos = new Persona("jhonatan velez", 31);
+carlos.mostrarInfo();
+
---
+++ src/modulos/mensajeAlerta.js
@@ -0,0 +1,11 @@
+
+
+const mensajeAlerta = (mensaje) => {
+    alert(mensaje)
+}
+
+const saludo = () => {
+    console.log("SALUDO!")
+}
+
+export { mensajeAlerta }
